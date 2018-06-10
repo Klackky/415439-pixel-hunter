9c0942b2afac5591333ac522e514a1df26667712
Natalia <natalia.volchatova@gmail.com>
2018-06-10 19:05:44 +0300

    small fix

--- js/utils/calc-points.js
+++ js/utils/calc-points.js
@@ -3,21 +3,21 @@
  *
  * @function calculatePoints
  * @param {array} answers array of user`s answers
- * @param {number} lifes remaining lifes
+ * @param {number} lives remaining lives
  * @return {number} final points
  */
-const calculatePoints = (answers, lifes) => {
+const calculatePoints = (answers, lives) => {
   const correctAnswersArray = answers.filter((answer) => answer.answer);
   const fastAnswersArray = answers.filter((answer) => answer.time < 10);
   const slowAnswersArray = answers.filter((answer) => answer.time > 20);
   let points = 0;
-  if (lifes < 1) {
+  if (lives < 1) {
     return -1;
   } else {
     points += ((correctAnswersArray.length) * 100);
     points += ((fastAnswersArray.length) * 50);
     points -= ((slowAnswersArray.length) * 50);
-    points += lifes * 50;
+    points += lives * 50;
   }
   return points;
 };
